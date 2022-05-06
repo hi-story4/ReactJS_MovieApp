70461901ccee2034e3b741efa6df37821ba35b40
jun-hyeok <wnsgur1503@naer.com>
2022-05-06 10:42:19 +0900

    Updates

---
+++ precache-manifest.b22271b75ea2ff5f022e4318a3c40483.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "877ba3f6c0c2f55df02e",
+    "url": "/ReactJS_MovieApp/static/css/main.47483c49.chunk.css"
+  },
+  {
+    "revision": "877ba3f6c0c2f55df02e",
+    "url": "/ReactJS_MovieApp/static/js/main.877ba3f6.chunk.js"
+  },
+  {
+    "revision": "6dd38acaeb737fa097d0",
+    "url": "/ReactJS_MovieApp/static/js/1.6dd38aca.chunk.js"
+  },
+  {
+    "revision": "0934c30726aef1ea4b59",
+    "url": "/ReactJS_MovieApp/static/js/runtime~main.0934c307.js"
+  },
+  {
+    "revision": "baa4e3bde04bc6df356b9b65537228c8",
+    "url": "/ReactJS_MovieApp/index.html"
+  }
+];
